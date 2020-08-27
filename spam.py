b018c48477f94d8440921b7e0d41aa7ccdbfc695
Naveen B Jacob <gamefreat@pop-os.localdomain>
2020-08-27 22:45:12 +0530

    object oriented

--- spam.py
+++ spam.py
@@ -1,35 +1,9 @@
-import os
-from collections import Counter
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split as tts
 from sklearn.metrics import accuracy_score
 import pickle as c
 from make_dict import make_Dict
-
-def make_dataset(dictionary):
-    direc = "emails/"
-    files = os.listdir(direc)
-
-    emails = [direc + email for email in files]
-
-    feature_set = []
-    labels = []
-    c = len(emails)
-
-    for email in emails:
-        data = []
-        f = open(email, encoding="latin-1")
-        words = f.read().split(' ')
-        for entry in dictionary:
-            data.append(words.count(entry[0]))     #taking count of each word in dictionary present in email
-        feature_set.append(data)       #storing data list
-        if "ham" in email:
-            labels.append(0)     #label not spam
-        if "spam" in email:
-            labels.append(1)     #label spam
-        print (c)
-        c -= 1
-    return feature_set, labels
+from make_dataset import make_Dataset
 
 def save(clf, name):
     with open(name, 'wb') as fp:
@@ -39,7 +13,9 @@ def save(clf, name):
 
 a = make_Dict()
 d = a.dict()
-features, labels = make_dataset(d)
+b = make_Dataset(d)
+features, labels = b.datas()
+# features, labels = make_dataset(d)
 
 #print (len(features), len(labels))
 
