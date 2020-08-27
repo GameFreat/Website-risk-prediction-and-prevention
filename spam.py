d7ebcc599e7d0dac2f40474431977e8746cf6f87
Naveen B Jacob <naveenbjacob123@gmail.com>
2020-08-27 23:17:52 +0530

    made it object oriented

--- spam.py
+++ spam.py
@@ -5,29 +5,26 @@ import pickle as c
 from make_dict import make_Dict
 from make_dataset import make_Dataset
 
-def save(clf, name):
-    with open(name, 'wb') as fp:
-        c.dump(clf, fp)
-    print ("saved")
+class spam:
 
+    def save(self,clf, name):
+        with open(name, 'wb') as fp:
+            c.dump(clf, fp)
+        print ("saved")
 
-a = make_Dict()
-d = a.dict()
-b = make_Dataset(d)
-features, labels = b.datas()
-# features, labels = make_dataset(d)
+    def __init__(self):
+        a = make_Dict()
+        d = a.dict()
+        b = make_Dataset(d)
+        features, labels = b.datas()
 
-#print (len(features), len(labels))
+        x_train, x_test, y_train, y_test = tts(features, labels, test_size=0.2)
 
-x_train, x_test, y_train, y_test = tts(features, labels, test_size=0.2)
+        clf = MultinomialNB()
+        clf.fit(x_train, y_train)     #training
 
-clf = MultinomialNB()
-clf.fit(x_train, y_train)     #training
-
-preds = clf.predict(x_test)
-print (accuracy_score(y_test, preds))
-save(clf, "text-classifier.mdl")     #storing trained file
-
-
-    
+        preds = clf.predict(x_test)
+        print (accuracy_score(y_test, preds))
+        self.save(clf, "text-classifier.mdl")     #storing trained file
+a = spam()
 
